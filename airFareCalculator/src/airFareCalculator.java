0067a609abc2e8661b471dab2d17b832ebf4c632
Arda Canbakis <108702450+ardacanbakis@users.noreply.github.com>
2024-01-13 18:52:53 +0300

    Update airFareCalculator.java
    updated

--- airFareCalculator/src/airFareCalculator.java
+++ airFareCalculator/src/airFareCalculator.java
@@ -6,45 +6,48 @@ public class airFareCalculator {
 
     public static void main(String[] args) {
 
-        // getting input from the user
-
-        Scanner userDistance = new Scanner(System.in);
+    //variables
+        double pricePerKm = 0.1;
+        double distance;
+        int age;
+        int travelType;
+        double ticketPrice;
+        double ageDiscount = 1;
+        double travelTypeDiscount = 1;
+        
+        Scanner scanner = new Scanner(System.in);
+        
         System.out.println("Please enter the distance you want to travel in KMs : ");
-        double distance = userDistance.nextDouble();
+        distance = scanner.nextDouble();
 
-        Scanner userAge = new Scanner(System.in);
         System.out.print("Please enter your age : ");
-        int age = userAge.nextInt();
+        age = scanner.nextInt();
 
-        Scanner userTravelType = new Scanner(System.in);
         System.out.println("Please indicate your travel type. 1 = One-Way | 2 = Round trip");
-        int travelType = userTravelType.nextInt();
+        travelType = scanner.nextInt();
+
 
         if ((distance < 1) || (age < 1) || (travelType != 1 && travelType != 2)) {
             System.out.println("Wrong input, please try again.");
-        } else {
+        }
 
-            double pricePerKm = 0.1;
-            double ticketPrice = pricePerKm * distance;
-            double ageDiscount = 1;
-            double travelTypeDiscount = 1;
+        ticketPrice= pricePerKm * distance;
 
-            // apply age discount if available
-
-            // any way to make it better ?
-
+    // Age discount
             if (age < 12) {
                 ageDiscount = 0.5;
             } else if (12 <= age && age <= 24) {
             ageDiscount = 0.9;
             } else if (age >65) {
             ageDiscount = 0.7;
-    }
-            // apply %20 off if round trip
-        if (travelType == 2 ) {
+            }
+    // apply %20 off if round trip
+            if (travelType == 2 ) {
+            ticketPrice = ticketPrice * 2; //*  by 2 cause round trip
             travelTypeDiscount = 0.8;
-        }
-            //calculate final price
+            }
+
+    //calculate final price
             double finalTicketPrice = ticketPrice * ageDiscount * travelTypeDiscount;
 
             System.out.println
@@ -55,4 +58,4 @@ public class airFareCalculator {
                             "Ticket price after discounts : " + finalTicketPrice + "$");
         }
     }
-}
+

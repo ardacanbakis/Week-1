0e204d16fb950fd029efe340498e7120f4beaae6
Arda Canbakis <108702450+ardacanbakis@users.noreply.github.com>
2024-02-13 19:47:09 +0300

    v.1
    v.1

---
+++ harmonicCalculator/src/Main.java
@@ -0,0 +1,26 @@
+// a simple java application to calculate the harmonic series of a user given number
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Get a number from the user
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Calculate the harmonic series
+        double harmonicSeries = 0;
+
+        for (int i = 1; i <= number; i++) {
+            harmonicSeries += 1.0 / i;
+        }
+
+        // Print the harmonic series
+        System.out.println("Harmonic series of " + number + " is: " + harmonicSeries);
+
+        // Close the scanner
+        scanner.close();
+    }
+}
